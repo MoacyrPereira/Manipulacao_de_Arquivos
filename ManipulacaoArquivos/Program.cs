996a53d10becdcf4bd2cd79b014774ad6b201700
Moacyr Pereira <moacyrpereira10@hotmail.com>
2021-09-21 11:18:51 -0300

    Projeto de Manipulação de listas

---
+++ ManipulacaoArquivos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using ManipulacaoArquivos.Entidades;
+
+namespace ManipulacaoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o caminho completo do arquivo: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath); //Le tudo no diretorio
+
+                //Criar pasta e arquivo
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+
+                        Produto prod = new Produto(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro!!" + e.Message);
+
+            }
+
+        }
+    }
+}
